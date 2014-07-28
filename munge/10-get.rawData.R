1ad913f7456aca0b342ef845dfa6102894841d79
gabx <arnaud.gaboury@gmail.com>
2014-07-28 20:38:34 +0200

    correct to read.table

--- munge/10-get.rawData.R
+++ munge/10-get.rawData.R
@@ -5,15 +5,15 @@
 cat('\nDownloading, unzipping project and data.Please wait')
 
 # read the url file as a table
-url <- data.table('DataSet.url')
+url <- read.table('DataSet.url')
 
 # download data set
-download.file(url[,1], destfile = './data/DataSet.zip', quiet = T, method='curl')              
-unzip('./data/DataSet.zip', exdir = './data')
+download.file(url[,1], destfile = 'data/DataSet.zip', quiet = T, method='curl')              
+unzip('data/DataSet.zip', exdir = 'data')
 
 # do some cleaning
-file.remove('./data/DataSet.zip')
+file.remove('data/DataSet.zip')
 file.remove('master.zip')
 
-if (file.exists('./data/UCI HAR Dataset/'))
+if (file.exists('data/UCI HAR Dataset/'))
 {cat("Downloading and unzipping has completed succesfuly")}
