4cce7947788384e4dc28c8c6a0332df7dffecf80
gabx <arnaud.gaboury@gmail.com>
2014-07-29 00:05:45 +0200

    add missing arg to unzip

--- munge/10-get.rawData.R
+++ munge/10-get.rawData.R
@@ -9,7 +9,7 @@ url <- read.table('DataSet.url')
 
 # download data set
 download.file(url[1,], destfile = 'data/DataSet.zip', quiet = T, method='curl')              
-unzip('data/DataSet.zip')
+unzip('data/DataSet.zip', exdir = 'data')
 
 # do some cleaning
 file.remove('data/DataSet.zip')
