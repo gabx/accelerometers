b3775904c43bd66e618956825079cc84ef2facc1
gabx <arnaud.gaboury@gmail.com>
2014-07-28 15:30:01 +0200

    first one

--- munge/30-write.dataSet.R
+++ munge/30-write.dataSet.R
@@ -1,20 +1,23 @@
 # write tidy data set
 
+
 cat('\nWriting Tidy Data to .csv file')
 
-setwd('../')
+write.csv(my.result1,'data/Mean_Std_per_activity.csv')
 
-write.csv(my.result1,'Mean_Std_per_activity.csv')
-
-write.csv(my.result2,'Mean_Std_per_activity_and_subject.csv')
+write.csv(my.result2,'data/Mean_Std_per_activity_and_subject.csv')
 
 cat('\nPlease find Mean_Std_per_activity.csv & Mean_Std_per_activity.csv
 in the Data directory')
         
     
 # write codebook
-# create .tex file
+cat('\nWriting the codebook pdf')
 
-# knit2pdf('codebook.Rnw')
+knit2pdf('munge/codebook.Rnw','doc/codebook.tex')
+
+cat('\nPlease find codebook.pdf in the doc directory.\
+Everything is done. Exiting the project !')
+
 
 
