a19154a94e79cef9dcb349bfd0d966aa6cd43058
gabx <arnaud.gaboury@gmail.com>
2014-07-28 20:08:26 +0200

    read .url file as a table

--- munge/10-get.rawData.R
+++ munge/10-get.rawData.R
@@ -4,22 +4,23 @@
 
 cat('\nDownloading, unzipping project and data.Please wait')
 
+# read the url file as a table
+url <- data.table('DataSet.url')
+
 # download project from github
-url <-  scan('DataSet.url', what = list(""), skip = 1)
-download.file(url, destfile = 'master.zip', quiet = T, method = 'curl')
+download.file(url[,2], destfile = 'master.zip', quiet = T, method = 'curl')
               
 unzip('master.zip')
 file.remove('master.zip')
 
 # download data set
-url <-  scan('DataSet.url', what = list(""), n = 1)
-download.file(url, destfile = './data/DataSet.zip', 
+download.file(url[,1], destfile = './data/DataSet.zip', 
               quiet = T, method='curl')
 unzip('./data/DataSet.zip', exdir = './data')
 
 # do some cleaning
 file.remove('./data/DataSet.zip')
-rm('url')
+
 
 if (file.exists('./data/UCI HAR Dataset/'))
 {cat("Downloading and unzipping has completed succesfuly")}
