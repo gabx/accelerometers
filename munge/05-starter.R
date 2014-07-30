e030cb347f781da89f6ab434c61c5b0d4844c46f
gabx <arnaud.gaboury@gmail.com>
2014-07-30 11:28:27 +0200

    test if download links exist

---
+++ munge/05-starter.R
@@ -0,0 +1,19 @@
+# start profiling
+file.create("profiling/profile.out")
+Rprof("profiling/profile.out", line.profiling=TRUE, interval = 0.01 )
+
+# store the current working directory
+cur.dir <- getwd()
+
+# Test if url exists
+url <- read.table('DataSet.url')
+Exist.Url <- as.data.table(url[by(url[,], seq_len(nrow(url)), url.exists),])
+True.Url <- ((dim(url) == dim(Exist.Url)))
+Non.Exist.Url <- url[True.Url,]
+
+if ((True.Url == FALSE){
+    cat("\nWrong URL. Please verify\n",
+         cat(Non.Exist.Url)) 
+        
+
+    
