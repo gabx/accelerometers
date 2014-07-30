65a6437984c2264adc1ed3896cd25223004b966d
gabx <arnaud.gaboury@gmail.com>
2014-07-30 17:46:30 +0200

    change script name and add load initial image

--- munge/35-end.R
+++ munge/35-end.R
@@ -1,7 +1,12 @@
-# script to clean directories
-
-setwd(cur)
-
+# clean directories
 unlink('data/UCI HAR Dataset', recursive = T)
 file.remove('doc/codebook.aux', 'doc/codebook.log', 'doc/codebook.out',
             'doc/codebook.tex', 'doc/codebook.toc')
+
+# load initial workspace
+load(cache/init.env.RData)
+
+# back to initial directory
+setwd(cur)
+
+
