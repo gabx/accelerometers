f2fa8409ce15160d283ff223d9e669a40e7129ad
gabx <arnaud.gaboury@gmail.com>
2014-07-31 11:16:29 +0200

    use Url form 05-starter then rm it

--- munge/10-get.rawData.R
+++ munge/10-get.rawData.R
@@ -4,18 +4,17 @@
 
 cat('\nDownloading, unzipping project and data files. Please wait\n\n')
 
-# read the url file as a table
-url <- read.table('DataSet.url')
 
 # download data set if not downloaded
 if (! file.exists('./data/DataSet.zip')){
-    download.file(url[1,], destfile = 'data/DataSet.zip', quiet = T, method='curl')
+    download.file(Url[1,], destfile = 'data/DataSet.zip', quiet = T, method='curl')
 }
               
 unzip('data/DataSet.zip', exdir = 'data')
 
 
 if (file.exists('./data/UCI HAR Dataset/')) {
+    rm(Url)
     cat("\nDownloading and unzipping has completed succesfuly\n\n")
 } else {
     stop("\nDownloading has failed. Stop")
