8c163b1a6ea90237e08372e48240096ced0fd2fc
gabx <arnaud.gaboury@gmail.com>
2014-07-31 22:34:50 +0200

    add helper function

--- lib/helpers.R
+++ lib/helpers.R
@@ -1,4 +1,14 @@
-helper.function <- function()
+#helper.function <- function()
+#{
+#  return(1)
+#}
+
+my.rt <- function(x,...)
 {
-  return(1)
+    # apply read.table to txt files if data table is not already cached
+    # x is a character vector
+    y <- gsub(".txt", ".dt", x, fixed = T)
+    if (y %in% ls() == FALSE){
+        rt <- read.table(x, header = F, sep = "", dec = '.') 
+    }        
 }
