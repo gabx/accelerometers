8a577d298694b7751b76dd6bcc4af22daf71fc57
gabx <arnaud.gaboury@gmail.com>
2014-08-01 11:49:49 +0200

    don't show warnings when removing files

--- munge/35-end.R
+++ munge/35-end.R
@@ -1,7 +1,7 @@
 # clean directories
 unlink('data/UCI HAR Dataset', recursive = T)
 file.remove('doc/codebook.aux', 'doc/codebook.log', 'doc/codebook.out',
-            'doc/codebook.tex', 'doc/codebook.toc')
+            'doc/codebook.tex', 'doc/codebook.toc', showWarnings = FALSE)
 
 ## restore the saved values to the user's workspace
 load("cache/init.all.RData", .GlobalEnv)
